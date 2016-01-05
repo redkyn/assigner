df0d69d886b406ab63e6b34144cadfc590bf54ca
Ty Morrow <tjm6f4@mst.edu>
2016-01-05 13:14:29 -0600

    Repo creation via roster JSON file.

--- assign.py
+++ assign.py
@@ -4,6 +4,7 @@ import json
 import getpass
 import gitlab
 
+logging.getLogger("requests").setLevel(logging.WARNING)
 logging.basicConfig(format=':: %(levelname)s: %(message)s', level=logging.DEBUG)
 
 print('Setup')
@@ -42,15 +43,18 @@ except Exception as e:
         logging.error(e)
     sys.exit(1)
 
-# Load Roster
+# Load roster from JSON file
 try:
     with open(args.roster, encoding='utf-8') as dataFile:
         roster = json.loads(dataFile.read())
-    students = roster['students']
+    sections = roster['sections']
+    studentCount = 0
+    for sec in sections:
+        studentCount += len(sec['students'])
 except Exception as e:
     logging.error('Failed to open roster file with provided name.')
     sys.exit(1)
-logging.info('Roster: {0} (count: {1})'.format(args.roster, str(len(students))))
+logging.info('Roster loaded: {0} (sections: {1}, students: {2})'.format(args.roster, str(len(sections)), str(studentCount)))
 
 print('GitLab Authentication')
 # Get user's GitLab info
@@ -58,10 +62,36 @@ userName = input('> GitLab User Name: ')
 userPw = getpass.getpass('> GitLab Password: ')
 # Connect to GitLab
 try:
-    glHost = gitlab.Gitlab(host)
-    glHost.login(userName, userPw)
+    git = gitlab.Gitlab(host)
+    git.login(userName, userPw)
 except Exception as e:
     logging.error('GitLab authentication failed.  Please check your credentials.')
     sys.exit(1)
 
 logging.info('Authentication succcessful!')
+
+# Get group ID
+for g in git.getall(git.getgroups):
+    if g['name'] == group:
+        groupId = g['id']
+
+allUsers = git.getall(git.getusers)
+
+# Create an assignment repo for each student
+print('Repository Creation & Permissions')
+for sec in sections:
+    logging.info('Creating repos for students in section ' + sec['name'])
+    studentsAdded = 0
+    for student in sec['students']:
+        projectName = '{0}-{1}-{2}'.format(assignment.rstrip('.git'), sec['name'], student['username'])
+        git.createproject(projectName, namespace_id=groupId, visibility_level=0)
+        project = git.getproject(group + '/' + projectName)
+        for u in allUsers:
+            if u['username'] == student['username']:
+                git.addprojectmember(project['id'], u['id'], 30) # Developer = 30
+                studentsAdded += 1
+                break
+    logging.info('Done, ' + str(studentsAdded) + ' assignment(s) made')
+
+# Clone base repo and push to each repository that was just created
+print('Push Initial Code')
