c9ee5d6c264f2904cd3b94ff09c03dc259f8e27e
Ty Morrow <tjm6f4@mst.edu>
2016-01-06 18:25:33 -0600

    Responded to Nate's feedback; refactored messages.

--- assigner.py
+++ assigner.py
@@ -10,149 +10,149 @@ import sys
 from urllib.parse import urlparse
 import yaml
 
+def exit_with_error(msg):
+    logging.error(msg)
+    sys.exit(1)
+
 CONFIG_FILE_NAME = "_config.yml"
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.basicConfig(format=":: %(levelname)s: %(message)s", level=logging.DEBUG)
 
-print("Setup")
+print("[Assigner]")
+
+print("\nParsing URL Arguments")
 # Get commandline arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('hwrepo', help="URL to repository where assignment exists")
-parser.add_argument('roster', help="File name for class roster")
+parser.add_argument("hwrepo", help="URL to repository where assignment exists")
+parser.add_argument("roster", help="File name for class roster")
 args = parser.parse_args()
 
 # Parse URL
 parsed_url = urlparse(args.hwrepo)
 path_entries = parsed_url.path.split('/')
+if len(path_entries) < 3:
+    exit_with_error(
+        "The provided URL is invalid. \n" +
+        "Required format: [hostname]/[group]/[repo].git")
+# Get host name
 if parsed_url.netloc:
     host = parsed_url.netloc
 else:
     host = path_entries[0]
+if not host:
+    exit_with_error("Host part was not found in the provided URL")
 logging.info("Host: " + host)
-
-try: # Is there a reason this is done with a try-catch instead of an if-else?
-    group = path_entries[1]
-    if not group:
-        raise Exception
-    logging.info("Group: " + group)
-except Exception as e:
-    logging.error("Group part was not found in the provided URL")
-    sys.exit(1)
-    
-try: # Ditto here
-    assignment = path_entries[2]
-    if not assignment:
-        raise Exception
-    if not assignment.endswith('.git'): # Why raise an exception over this if you're just going to strip it anyway?
-        raise Exception("Assignment must end with .git identifier")
-    assignment_name = assignment.rstrip('.git') # This will happily strip any hanging ., g, i, or t from the end of the assignment name. Ex: "asdfg.git".rstrip(".git") -> "asdf". Better way is to just chop the last four chars off the end of assignment.
-    logging.info("Assignment: " + assignment_name)
-except Exception as e:
-    if not e:
-        logging.error("Assignment part was not found in provided URL")
-    else:
-        logging.error(e)
-    sys.exit(1)
+# Get group name
+group_name = path_entries[1]
+if not group_name:
+    exit_with_error("Group part was not found in the provided URL")
+logging.info("Group: " + group_name)
+# Get assignment name
+assignment = path_entries[2]
+if not assignment:
+    exit_with_error("Assignment part was not found in provided URL")
+if not assignment.endswith('.git'):
+    exit_with_error("Assignment must end with .git identifier")
+assignment_name = assignment[:-4]
+logging.info("Assignment: " + assignment_name)
 
 # Create/load config file
 if not os.path.isfile(CONFIG_FILE_NAME):
     logging.info("Creating new config file")
     with open(CONFIG_FILE_NAME, 'w') as f:
         f.write("private_token: \n")
-logging.info("Loading config file")
 with open(CONFIG_FILE_NAME, 'r') as f:
     config = yaml.load(f)
-if not config["private_token"]: #nit: if "private_token" not in config: reads better to me
-    logging.warning("""
-        No GitLab private token has been set in {}.
-        Add it in order to speed up authentication.
-        """.format(CONFIG_FILE_NAME))
+logging.info("Config loaded")
 
 # Load roster from JSON file
 try:
-    with open(args.roster, encoding='utf-8') as dataFile:
+    with open(args.roster, encoding="utf-8") as dataFile:
         roster = json.loads(dataFile.read())
-    sections = roster['sections']
-    studentCount = 0
-    for sec in sections:
-        studentCount += len(sec['students'])
-except Exception as e: #I would catch this as close to where it's thrown as possible. Also, you can just do 'except' if you don't care about what the exception was.
-    logging.error("Failed to open roster file with provided name.")
-    sys.exit(1)
+except Exception as e:
+    exit_with_error(e)
+if "sections" not in roster or not roster["sections"]:
+    exit_with_error("No sections defined in roster file")
+studentCount = 0
+for sec in roster["sections"]:
+    studentCount += len(sec["students"])
 logging.info(
-    "Roster loaded: {} (sections: {}, students: {})".format(
-        args.roster, str(len(sections)), str(studentCount)))
+    "Roster loaded: {} (section(s): {}, student(s): {})".format(
+        args.roster, str(len(roster["sections"])), str(studentCount)))
+if not studentCount:
+    logging.info("Exiting because no students were found in roster file")
+    sys.exit(1)
 
-print("GitLab Authentication")
+print("\nGitLab Authentication")
 try:
-    if config["private_token"]: #nit, use 'in' 
-        logging.info("Private key found.")
+    if config["private_token"]:
+        logging.info("Private key found")
         glab = gitlab.Gitlab(host, token=config["private_token"])
     else:
-        logging.info("No private key found.")
+        logging.info("No private key found, add it to speed up authentication")
         glab = gitlab.Gitlab(host)
         userName = input('> GitLab User Name: ')
         userPw = getpass.getpass('> GitLab Password: ')
         glab.login(userName, userPw)
-except Exception as e: #nit, just use 'except' if you don't care about e
-    logging.error("""
-        GitLab authentication failed.
-        Please check your credentials.""")
-    sys.exit(1)
-logging.info("Authentication succcessful!")
+except:
+    exit_with_error("GitLab authentication failed - check your credentials")
+logging.info("Authentication succcessful")
 
 # Get group ID
-for g in glab.getall(glab.getgroups): # This is going to get really slow if there are a lot of groups. Is it possible to search by group name?
-    if g['name'] == group:
-        group_id = g['id']
-        break
-
-allUsers = glab.getall(glab.getusers)
+group = glab.getgroups(group_name)
+group_id = group["id"]
 all_remotes = []
 
-print("Repository Creation & Permissions")
+print("\nRepository Creation & Permissions")
 # Create an assignment repo for each student
-for sec in sections:
-    logging.info("Creating repos for students in section " + sec['name'])
+for sec in roster["sections"]:
+    logging.info("Creating repos for students in section " + sec["name"])
     repos_made = 0
-    for student in sec['students']:
-        project_name = assignment_name + '-'
-        project_name += sec["name"] + '-'
+    for student in sec["students"]:
+        project_name = assignment_name + "-"
+        project_name += sec["name"] + "-"
         project_name += student["username"]
         glab.createproject(
             project_name,
-            namespace_id = group_id,
-            visibility_level = 0)
-        project = glab.getproject(group + '/' + project_name)
-        for u in allUsers: # This seems expensive, especially if you're looping over 5,000 users for each student!
-            if u['username'] == student['username']:
-                # Developer = 30
-                glab.addprojectmember(project['id'], u['id'], 30)
-                repos_made += 1
-                # Add url to remote list for use later
-                url = "git@{}:{}/{}.git".format(host, group, project_name)
-                all_remotes.append((student["username"], url.lower()))
-                break
-    logging.info('Done, ' + str(repos_made) + ' assignment(s) made')
+            namespace_id=group_id,
+            visibility_level=0)
+        project = glab.getproject(group_name + "/" + project_name)
+        # Get student GitLab id
+        matching_users = glab.getusers(student["username"])
+        if len(matching_users) != 1:
+            logging.warning(
+                "Student {} {} with username '{}' not found on GitLab".format(
+                    student["firstname"],
+                    student["lastname"],
+                    student["username"]))
+            continue;
+        user = matching_users[0]
+        # Developer = 30
+        glab.addprojectmember(project["id"], user["id"], 30)
+        repos_made += 1
+        # Add url to remote list for use later
+        url = "git@{}:{}/{}.git".format(host, group_name, project_name)
+        all_remotes.append((student["username"], url.lower()))
+    logging.info("Done, " + str(repos_made) + " repo(s) made")
 
-print("Push Initial Code")
 # Clone base repo and push to each repository that was just created
 try:
     if os.path.isdir(assignment_name):
-        shutil.rmtree(assignment_name) #careful! don't want to blow away stuff if you run this in the wrong directory. 
+        shutil.rmtree(assignment_name)
     local_repo = Repo.clone_from(args.hwrepo, assignment_name)
 except Exception as e:
-    logging.error("""
-        Failed to clone remote repo.
-        Please check your URL and credentials.""")
-    sys.exit(1)
+    exit_with_error("Failed to clone repo - check your URL and credentials")
+logging.info("Cloned base repo")
 
 # Push copy to each remote URL
 for name, remote in all_remotes:
     r = Remote.add(local_repo, name, remote)
     r.push("master")
+logging.info("Base repo pushed to all student repos")
 
-print("Cleaning Up")
+print("\nClean Up")
 # Delete local repository
-logging.info("Delete clone assignment repository")
 shutil.rmtree(assignment_name)
+logging.info("Deleted local copy of base repo")
+
+print("\n[Done]")
