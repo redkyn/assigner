e16bda7ceca99aa961f8128bd9f470a081affc48
Michael Wisely <michaelwisely@gmail.com>
2017-02-03 10:39:02 -0600

    Move assigner requirements to setup.py

--- src/setup.py
+++ src/setup.py
@@ -8,22 +8,13 @@ setup(  # pragma: no cover
     packages=['assigner'],
     package_dir={'assigner': 'assigner'},
     install_requires=[
-        'GitPython==1.0.1',
-        'PyYAML==3.11',
-        'colorlog==2.6.0',
-        'flake8==2.5.1',
-        'gitdb==0.6.4',
-        'jsonschema==2.5.1',
-        'mccabe==0.3.1',
-        'pep8==1.5.7',
-        'pyapi-gitlab==7.8.5',
-        'pyflakes==1.0.0',
         'requests==2.9.1',
-        'six==1.10.0',
-        'smmap==0.9.0',
-        'wheel==0.24.0',
+        'colorlog==2.6.0',
         'PTable==0.9.2',
-        'progressbar2==3.10.1'
+        'progressbar2==3.10.1',
+        'jsonschema==2.5.1',
+        'PyYAML==3.11',
+        'GitPython==1.0.1'
     ],
     entry_points={
         'console_scripts': ['assigner = assigner:run'],
