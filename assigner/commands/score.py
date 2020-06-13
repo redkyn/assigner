667e041a3d9f9275082a7255a9b19803c90f86c2
Josh Essman <josh@essman.me>
2020-06-13 15:41:05 -0500

    improvement: added argument and conditional check for modified files, added type hints

--- assigner/commands/score.py
+++ assigner/commands/score.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+from typing import Any, Dict, List, Optional, Tuple
 
 from requests.exceptions import HTTPError
 
@@ -6,14 +8,16 @@ from redkyn.canvas import CanvasAPI
 from redkyn.canvas.exceptions import CourseNotFound, AssignmentNotFound
 
 from assigner import make_help_parser
-from assigner.backends.base import RepoError
+from assigner.backends.base import RepoError, RepoBase, BackendBase
 from assigner.backends.decorators import requires_config_and_backend
 from assigner.roster_util import get_filtered_roster
 from assigner import progress
+from assigner.config import Config
 
 help = "Retrieves scores from CI artifacts and optionally uploads to Canvas"
 
 logger = logging.getLogger(__name__)
+
 
 def parse_assignment_name(name: str) -> str:
     """Removes the tail of a string starting with the
@@ -28,9 +32,12 @@ def parse_assignment_name(name: str) -> str:
         idx += 1
     return name[:idx]
 
-def get_most_recent_score(repo, student, result_path: str) -> str:
+
+def get_most_recent_score(
+    repo: RepoBase, student: Dict[str, Any], result_path: str
+) -> float:
     try:
-        logger.info("Scoring %s...", repo.name)
+        logger.info("Scoring %s...", repo.name_with_namespace)
         ci_jobs = repo.list_ci_jobs()
         most_recent_job_id = ci_jobs[0]["id"]
         score_file = repo.get_ci_artifact(most_recent_job_id, result_path)
@@ -38,15 +45,21 @@ def get_most_recent_score(repo, student, result_path: str) -> str:
             score = score_file.split()[-1]
         else:
             score = str(score_file)
-        return score
     except HTTPError as e:
         if e.response.status_code == 404:
-            logger.warning("CI artifact does not exist for %s in repo %s.", student["username"], repo.name)
-        else:
-            raise
+            logger.warning(
+                "CI artifact does not exist for %s in repo %s.",
+                student["username"],
+                repo.name_with_namespace,
+            )
+        raise
+    return float(score)
 
-def lookup_canvas_ids(conf, canvas, hw_name):
-    if 'canvas-courses' not in conf:
+
+def lookup_canvas_ids(
+    conf: Config, canvas: CanvasAPI, hw_name: str
+) -> Tuple[Dict[str, int], Dict[str, int]]:
+    if "canvas-courses" not in conf:
         logger.error(
             "canvas-course configuration is missing! Please set the "
             "Canvas course ID token before attempting to upload scores "
@@ -65,17 +78,26 @@ def lookup_canvas_ids(conf, canvas, hw_name):
             logger.error("Failed to pull assignment list from Canvas")
             raise AssignmentNotFound
         if len(canvas_assignments) != 1:
-            logger.error("Could not uniquely identify Canvas assignment from name %s and section %s", min_name, section)
+            logger.error(
+                "Could not uniquely identify Canvas assignment from name %s and section %s",
+                min_name,
+                section,
+            )
             raise AssignmentNotFound
         assignment_ids[section] = canvas_assignments[0]["id"]
     return (section_ids, assignment_ids)
 
-def student_search(roster, query):
+
+def student_search(
+    roster: List[Dict[str, Any]], query: str
+) -> Optional[Dict[str, Any]]:
     candidate_students = []
     # Search through the entire class for a match
     for student in roster:
-        if query.lower() in student['name'].lower().replace(',', '') or \
-           query.lower() in student['username']:
+        if (
+            query.lower() in student["name"].lower().replace(",", "")
+            or query.lower() in student["username"]
+        ):
             candidate_students.append(student)
 
     if not candidate_students:
@@ -86,14 +108,24 @@ def student_search(roster, query):
     else:
         ct = 0
         for cand_user in candidate_students:
-            print('{}: {}, {}'.format(ct, cand_user['name'], cand_user['username']))
+            print("{}: {}, {}".format(ct, cand_user["name"], cand_user["username"]))
             ct += 1
         selected = -1
         while selected < 0 or selected >= len(candidate_students):
-            selected = int(input('Enter the number of the correct student: '))
+            selected = int(input("Enter the number of the correct student: "))
         return candidate_students[selected]
 
-def handle_scoring(conf, backend, args, student, canvas, section_ids, assignment_ids, upload=True) -> float:
+
+def handle_scoring(
+    conf: Config,
+    backend: BackendBase,
+    args: argparse.Namespace,
+    student: Dict[str, Any],
+    canvas: CanvasAPI,
+    section_ids: Dict[str, Any],
+    assignment_ids: Dict[str, Any],
+    upload: bool = True,
+) -> Optional[float]:
     hw_name = args.name
     result_path = args.path
     namespace = conf.namespace
@@ -101,8 +133,9 @@ def handle_scoring(conf, backend, args, student, canvas, section_ids, assignment
     backend_conf = conf.backend
     username = student["username"]
     student_section = student["section"]
-    full_name = backend.student_repo.build_name(semester, student_section,
-                                                hw_name, username)
+    full_name = backend.student_repo.build_name(
+        semester, student_section, hw_name, username
+    )
     try:
         repo = backend.student_repo(backend_conf, namespace, full_name)
         if "id" not in student:
@@ -116,20 +149,24 @@ def handle_scoring(conf, backend, args, student, canvas, section_ids, assignment
             except Exception as e:
                 logger.debug(e)
                 logger.error("Unable to lookup Canvas account ID")
-                return
             try:
-                canvas.put_assignment_submission(course_id, assignment_id, student_id, score + "%")
+                canvas.put_assignment_submission(
+                    course_id, assignment_id, student_id, str(score) + "%"
+                )
             except Exception as e:
                 print(e)
                 logger.debug(e)
                 logger.warning("Unable to update submission for Canvas assignment")
-        return float(score)
     except RepoError as e:
         logger.debug(e)
         logger.warning("Unable to find repo for %s with URL %s", username, full_name)
+    return score
+
 
 @requires_config_and_backend
-def score_assignments(conf, backend, args):
+def score_assignments(
+    conf: Config, backend: BackendBase, args: argparse.Namespace
+) -> None:
     """Goes through each student repository and grabs the most recent CI
     artifact, which contains their autograded score
     """
@@ -148,15 +185,18 @@ def score_assignments(conf, backend, args):
     count = 0
     scores = []
     for student in progress.iterate(roster):
-        score = handle_scoring(conf, backend, args, student, canvas, section_ids, assignment_ids, upload)
+        score = handle_scoring(
+            conf, backend, args, student, canvas, section_ids, assignment_ids, upload
+        )
         if score is not None:
             scores.append(score)
 
     print("Scored {} repositories.".format(count))
     print(scores)
 
+
 @requires_config_and_backend
-def checkout_students(conf, backend, args):
+def checkout_students(conf: Config, backend: BackendBase, args: argparse.Namespace):
     """Interactively prompts for student info and grabs the most recent CI
     artifact, which contains their autograded score
     """
@@ -172,33 +212,37 @@ def checkout_students(conf, backend, args):
         return
 
     while True:
-        query = input('Enter student ID or name, or \'q\' to quit: ')
-        if query in 'quit':
+        query = input("Enter student ID or name, or 'q' to quit: ")
+        if query in "quit":
             break
         student = student_search(roster, query)
         if not student:
             continue
 
-        score = handle_scoring(conf, backend, args, student, canvas, section_ids, assignment_ids)
+        score = handle_scoring(
+            conf, backend, args, student, canvas, section_ids, assignment_ids
+        )
+
 
 @requires_config_and_backend
-def integrity_check(conf, backend, args):
+def integrity_check(conf: Config, backend: BackendBase, args: argparse.Namespace):
     """Checks that none of the grading files were modified in the timeframe
     during which students could push to their repository
     """
     hw_name = args.name
     student = args.student
+    files_to_check = args.files
     namespace = conf.namespace
     semester = conf.semester
     backend_conf = conf.backend
-    print("here 1")
     roster = get_filtered_roster(conf.roster, args.section, None)
 
     for student in progress.iterate(roster):
         username = student["username"]
         student_section = student["section"]
-        full_name = backend.student_repo.build_name(semester, student_section,
-                                                    hw_name, username)
+        full_name = backend.student_repo.build_name(
+            semester, student_section, hw_name, username
+        )
 
         try:
             repo = backend.student_repo(backend_conf, namespace, full_name)
@@ -206,53 +250,64 @@ def integrity_check(conf, backend, args):
             # Do some git stuff wit the commits
             # logger.warning("student %s modified a file", student['username'])
             for commit in commits:
-                print(repo.list_commit_files(commit["id"]))
+                for file in repo.list_commit_files(commit["id"]):
+                    if file in files_to_check:
+                        logger.warning(
+                            "student %s modified a file: %s", student["username"], file
+                        )
         except RepoError as e:
             logger.debug(e)
-            logger.warning("Unable to find repo for %s with URL %s", username, full_name)
+            logger.warning(
+                "Unable to find repo for %s with URL %s", username, full_name
+            )
 
-def setup_parser(parser):
-    subparsers = parser.add_subparsers(title='Scoring commands')
+
+def setup_parser(parser: argparse.ArgumentParser):
+    subparsers = parser.add_subparsers(title="Scoring commands")
 
     all_parser = subparsers.add_parser(
-        "all", help="Get scores (using CI artifacts) for all students for a given assignment"
+        "all",
+        help="Get scores (using CI artifacts) for all students for a given assignment",
     )
     # all_parser.set_defaults(run=score_assignments)
 
-    all_parser.add_argument("name",
-                        help="Name of the assignment to score")
-    all_parser.add_argument("--section", nargs=1,
-                        help="Section to score")
-    all_parser.add_argument("--student", nargs=1,
-                        help="ID of student to score")
-    all_parser.add_argument("--upload", action="store_true",
-                        help="Upload grades to Canvas")
-    all_parser.add_argument("--path", default="results.txt",
-                        help="Path within repo to grader results file")
+    all_parser.add_argument("name", help="Name of the assignment to score")
+    all_parser.add_argument("--section", nargs=1, help="Section to score")
+    all_parser.add_argument("--student", nargs=1, help="ID of student to score")
+    all_parser.add_argument(
+        "--upload", action="store_true", help="Upload grades to Canvas"
+    )
+    all_parser.add_argument(
+        "--path", default="results.txt", help="Path within repo to grader results file"
+    )
     all_parser.set_defaults(run=score_assignments)
 
     checkout_parser = subparsers.add_parser(
-        "checkout", help="Interactively checkout individual students and upload their grades to Canvas"
+        "checkout",
+        help="Interactively checkout individual students and upload their grades to Canvas",
     )
-    checkout_parser.add_argument("name",
-                        help="Name of the assignment to score")
-    checkout_parser.add_argument("--section", nargs="?",
-                        help="Section to score")
-    checkout_parser.add_argument("--path", default="results.txt",
-                        help="Path within repo to grader results file")
+    checkout_parser.add_argument("name", help="Name of the assignment to score")
+    checkout_parser.add_argument("--section", nargs="?", help="Section to score")
+    checkout_parser.add_argument(
+        "--path", default="results.txt", help="Path within repo to grader results file"
+    )
     checkout_parser.set_defaults(run=checkout_students)
 
     integrity_parser = subparsers.add_parser(
-        "integrity", help="Check the integrity of desired files for a set of assignment respositories"
+        "integrity",
+        help="Check the integrity of desired files for a set of assignment respositories",
     )
-    integrity_parser.add_argument("name",
-                        help="Name of the assignment to check")
-    integrity_parser.add_argument("--section", nargs=1,
-                        help="Section to check")
-    integrity_parser.add_argument("--student", nargs=1,
-                        help="ID of student to score")
+    integrity_parser.add_argument("name", help="Name of the assignment to check")
+    integrity_parser.add_argument("--section", nargs=1, help="Section to check")
+    integrity_parser.add_argument("--student", nargs=1, help="ID of student to score")
+    integrity_parser.add_argument(
+        "-f",
+        "--files",
+        nargs="+",
+        dest="files",
+        default=[],
+        help="Files to check for modification",
+    )
     integrity_parser.set_defaults(run=integrity_check)
 
-    make_help_parser(
-        parser, subparsers, "Show help for score or one of its commands"
-    )
+    make_help_parser(parser, subparsers, "Show help for score or one of its commands")
